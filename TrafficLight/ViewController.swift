718730ee9c137b1333d26ddf2d47a8d0f5a54336
Kamal <kamalios@mail.ru>
2023-02-02 20:33:52 +0300

    Added all elements on mainView Added constraints Added logic of work trafficLight in viewController

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum trafficLightStates {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var trafficLightStateCurrent = trafficLightStates.red
+    var trafficLightStateOn: CGFloat = 1
+    var trafficLightStateOff: CGFloat = 0.3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startButton.layer.cornerRadius = startButton.frame.width / 10
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
     }
 
-
+    
+    @IBAction func startButtonTapped() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch trafficLightStateCurrent {
+        case .red:
+            redLight.alpha = trafficLightStateOn
+            greenLight.alpha = trafficLightStateOff
+            trafficLightStateCurrent = trafficLightStates.yellow
+        case .yellow:
+            redLight.alpha = trafficLightStateOff
+            yellowLight.alpha = trafficLightStateOn
+            trafficLightStateCurrent = trafficLightStates.green
+        case .green:
+            yellowLight.alpha = trafficLightStateOff
+            greenLight.alpha = trafficLightStateOn
+            trafficLightStateCurrent = trafficLightStates.red
+        }
+        
+    }
+    
+    
 }
 
